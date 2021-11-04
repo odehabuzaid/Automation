3b71a1a5b5ed5a14829b6caa9df2fd710e092cad
Odeh <abuzaid163@hotmail.com>
2021-11-04 18:32:30 +0200

    resubmit

--- automation/main.py
+++ automation/main.py
@@ -15,34 +15,30 @@ def auto():
         
         regex = re.compile(formated_numbers)
         
-        
-        
         phone_numbers_ = \
             " ".join(regex.findall(content)) \
             .replace(')','-') \
             .replace('(','') \
             .replace('.','-')
-
-        
         
         phone_numbers = open("./results/phone_numbers.txt", "w+")
        
-        
-        
-        
+        from itertools import groupby
         phone_numbers_ = phone_numbers_.split()
         phone_numbers_.sort()
-        
         strs = " ".join(phone_numbers_)
-        from itertools import groupby
         phone_numbers_ = [num for num,v in groupby(strs.split())]
         
-        
+        email_address.sort()
         strs = " ".join(email_address)
+        
         email_address = [email for email,v in groupby(strs.split())]
+        
         emails = open("./results/emails.txt", "w+")
+
         for email in email_address: 
             emails.write(email + '\n')
+
         for num in phone_numbers_: 
             phone_numbers.write(num + '\n')
 auto()
